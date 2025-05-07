401594584ab8ff761bc2cd71898945e65cd892f4
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2025-05-07 07:51:12 +0700

    changes

--- app/Http/Controllers/ProfesiController.php
+++ app/Http/Controllers/ProfesiController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProfesiModel;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class ProfesiController extends Controller
 {
-    //
+
 }
