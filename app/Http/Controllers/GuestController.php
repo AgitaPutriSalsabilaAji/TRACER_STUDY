e9e0ba660ac78ec9dd5ee97d5cc74591945af43e
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-05-11 18:51:34 +0700

    coba form atasan

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -6,27 +6,3 @@ use Illuminate\Http\Request;
 
 class GuestController extends Controller
 {}
-//     public function showForm()
-//     {
-//         return view('inform');
-//     }
-
-//     public function submitForm(Request $request)
-//     {
-//         // Validasi data
-//         $request->validate([
-//             'no_telp' => 'required',
-//             'email' => 'required|email',
-//             'tgl_kerja' => 'required|date',
-//             'jenis_instansi' => 'required',
-//             'nama_instansi' => 'required',
-//             'lokasi' => 'required',
-//             'profesi' => 'required',
-//         ]);
-
-//         // Simpan atau proses data di sini jika dibutuhkan
-//         // Contoh: simpan ke database (jika ada modelnya)
-
-//         return back()->with('success', 'Data berhasil dikirim!');
-//     }
-// }
