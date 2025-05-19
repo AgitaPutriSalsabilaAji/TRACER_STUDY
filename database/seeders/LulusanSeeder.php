f0eec9a273d09a320b6747d17294a4c7cd891df1
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-05-19 15:14:24 +0700

    chore lulusan seeder

--- database/seeders/LulusanSeeder.php
+++ database/seeders/LulusanSeeder.php
@@ -2,49 +2,38 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
-use App\Models\Regency;
-use App\Models\Topic;
-use App\Models\Comment;
+use App\Models\Alumni;
+use App\Models\Lulusan;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
-class PostSeeder extends Seeder
+class LulusanSeeder extends Seeder
 {
     public function run()
     {
-        $faker = Faker::create('id_ID');
+        $faker = \Faker\Factory::create('id_ID');
 
-        $regencies = Regency::pluck('id')->toArray();
-        $topics = Topic::pluck('id')->toArray();
+        $alumniData = Alumni::take(40)->select('id', 'tanggal_lulus')->get();
 
-        for ($i = 0; $i < 50; $i++) {
-            $upvote = $faker->numberBetween(0, 1000);
-            $downvote = $faker->numberBetween(0, 500);
-
-            $post = Post::create([
-                'caption' => $faker->sentence,
-                'img_path' => $faker->imageUrl(640, 480, 'nature'),
-                'video_path' => null,
-                'upvote' => $upvote,
-                'downvote' => $downvote,
-                'clean_vote' => 0, 
-                'comment_count' => 0, 
-                'location' => $faker->randomElement($regencies),
-                'time_happening' => $faker->optional()->dateTimeBetween('-1 year', 'now'),
-                'geo_location' => $faker->latitude() . ',' . $faker->longitude(),
-                'topic_id' => $faker->randomElement($topics),
+        foreach ($alumniData as $alumni) {
+            $tahunLulus = Carbon::parse($alumni->tanggal_lulus)->year;
+            Lulusan::create([
+                'alumni_id' => $alumni->id,
+                'profesi_id' => rand(1, 17),
+                'jenis_instansi_id' => rand(1, 4),
+                'tahun_lulus' => $tahunLulus,
+                'no_hp' => $faker->phoneNumber,
+                'email' => $faker->unique()->safeEmail,
+                'tgl_pertama_kerja' => $faker->dateTimeBetween('2020-01-01', '2025-12-31')->format('Y-m-d'),
+                'tgl_mulai_kerja_instansi' => $faker->dateTimeBetween('2020-01-01', '2025-12-31')->format('Y-m-d'),
+                'nama_instansi' => $faker->company,
+                'skala' => $faker->randomElement(['Nasional', 'Internasional', 'Regional']),
+                'lokasi_instansi' => $faker->city,
+                'nama_atasan_langsung' => $faker->name,
+                'jabatan_atasan_langsung' => $faker->jobTitle,
+                'no_hp_atasan_langsung' => $faker->phoneNumber,
+                'email_atasan_langsung' => $faker->unique()->companyEmail,
             ]);
-
-            // Hitung clean_vote
-            $post->clean_vote = $upvote - $downvote;
-
-            // Hitung comment_count dari tabel comments
-            $countComments = Comment::where('post_id', $post->id)->count();
-
-            $post->comment_count = $countComments;
-
-            $post->save();
         }
     }
 }
